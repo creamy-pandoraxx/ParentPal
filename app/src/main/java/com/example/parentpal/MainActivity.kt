6e29b651e91c277e873b30f94ce88f2442739d1c
anjeliputeri <anjeliputeri92@gmail.com>
2023-05-29 20:22:31 +0700

    add Role Activity

--- app/src/main/java/com/example/parentpal/MainActivity.kt
+++ app/src/main/java/com/example/parentpal/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.parentpal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bRole = findViewById<Button>(R.id.btn_role)
+
+        bRole.setOnClickListener {
+            val goRole = Intent(this@MainActivity, RoleActivity::class.java)
+            startActivity(goRole)
+        }
     }
 }
---
+++ app/src/main/java/com/example/parentpal/RoleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.parentpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_role)
+    }
+}
