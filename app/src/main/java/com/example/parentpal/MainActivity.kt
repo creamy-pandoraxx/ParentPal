72aee3f082eb229271d57368d9ae7d15f0c5d509
Muhammad Adrian Zaky <71943576+creamy-pandoraxx@users.noreply.github.com>
2023-06-05 01:58:32 +0700

    update

--- app/src/main/java/com/example/parentpal/MainActivity.kt
+++ app/src/main/java/com/example/parentpal/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.parentpal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatDelegate
 
