4a3e3f480807bdfb72348b1d0d4e851a53961f32
anjeliputeri <anjeliputeri92@gmail.com>
2023-06-11 11:58:14 +0700

    test

--- app/src/main/java/com/example/parentpal/navfragment/BerandaFragment.kt
+++ app/src/main/java/com/example/parentpal/navfragment/BerandaFragment.kt
@@ -20,7 +20,7 @@ import com.example.parentpal.databinding.FragmentBerandaBinding
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
+//
 /**
  * A simple [Fragment] subclass.
  * Use the [BerandaFragment.newInstance] factory method to
