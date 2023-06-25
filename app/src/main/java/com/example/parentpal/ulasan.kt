e0456b9860d90b5a8e6d44f5a72d84355509c0a8
nikenutari <nikenutari852@gmail.com>
2023-06-26 01:02:14 +0700

    Ulasan dan Q & A (Done)

---
+++ app/src/main/java/com/example/parentpal/ulasan.kt
@@ -0,0 +1,11 @@
+package com.example.parentpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ulasan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ulasan)
+    }
+}
