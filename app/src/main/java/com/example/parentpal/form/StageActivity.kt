85cb0323d4b85d0267c8bc82b98dbea12a67188f
Muhammad Adrian Zaky <71943576+creamy-pandoraxx@users.noreply.github.com>
2023-06-27 09:57:44 +0700

    stage awal

--- app/src/main/java/com/example/parentpal/form/StageActivity.kt
+++ app/src/main/java/com/example/parentpal/form/StageActivity.kt
@@ -3,8 +3,10 @@ package com.example.parentpal.form
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.example.parentpal.R
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.parentpal.activity.HomeActivity
@@ -36,89 +38,26 @@ class StageActivity : AppCompatActivity() {
         bNext.setOnClickListener {
             val selectedId = radioGroup.checkedRadioButtonId
             val selectedRadioButton = findViewById<RadioButton>(selectedId)
-            val selectedStage = selectedRadioButton.text.toString()
+            val selectedStage= selectedRadioButton.text.toString()
             val stage = hashMapOf<String, Any>(
                 "stage" to selectedStage
             )
-
             val currentUser = auth.currentUser
             if (currentUser != null) {
                 val userEmail = currentUser.email.toString()
-                db.collection("mobile_users").document(userEmail).get()
-                    .addOnSuccessListener { document ->
-                        if (document != null && document.exists()) {
-                            // Jika pengguna sudah mengisi aktivitas ini sebelumnya, lanjutkan ke aktivitas selanjutnya
-                            navigateToNextActivity(selectedId)
-                        } else {
-                            // Simpan pilihan stage ke Firestore jika belum mengisi sebelumnya
-                            db.collection("mobile_users").document(userEmail)
-                                .set(stage, SetOptions.merge())
-                                .addOnSuccessListener {
-                                    navigateToNextActivity(selectedId)
-                                }
-                                .addOnFailureListener { exception ->
-                                    // Menangani kesalahan saat menyimpan data
-                                    Toast.makeText(this@StageActivity, "Gagal menyimpan data", Toast.LENGTH_SHORT).show()
-                                    Log.e("StageActivity", "Error saving data: ${exception.message}")
-                                }
-                        }
-                    }
-                    .addOnFailureListener { exception ->
-                        // Menangani kesalahan saat memeriksa data
-                        Toast.makeText(this@StageActivity, "Gagal memeriksa data", Toast.LENGTH_SHORT).show()
-                        Log.e("StageActivity", "Error checking data: ${exception.message}")
-                    }
+                db.collection("mobile_users").document(userEmail).set(stage, SetOptions.merge())
             }
-        }
 
-        checkPreviousStageActivity()
-    }
+            if (selectedId == R.id.rb_calon) {
+                // Navigasi ke Activity Main
+                val intent = Intent(this, LandingActivity::class.java)
+                startActivity(intent)
 
-    private fun checkPreviousStageActivity() {
-        val db = Firebase.firestore
-        val auth = FirebaseAuth.getInstance()
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            val userEmail = currentUser.email.toString()
-            db.collection("mobile_users").document(userEmail).get()
-                .addOnSuccessListener { document ->
-                    if (document != null && document.exists()) {
-                        // Jika pengguna sudah mengisi aktivitas ini sebelumnya, langsung beralih ke aktivitas selanjutnya
-                        val selectedId = document.getString("stage")?.let { getSelectedRadioButtonId(it) }
-                        val intent = Intent(this@StageActivity, HomeActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                }
-                .addOnFailureListener { exception ->
-                    // Menangani kesalahan saat memeriksa data
-                    Toast.makeText(this@StageActivity, "Gagal memeriksa data", Toast.LENGTH_SHORT).show()
-                    Log.e("StageActivity", "Error checking data: ${exception.message}")
-                }
-        }
-    }
-
-    private fun navigateToNextActivity(selectedId: Int?) {
-        if (selectedId == R.id.rb_calon) {
-            // Navigasi ke Activity Main
-            val intent = Intent(this, LandingActivity::class.java)
-            startActivity(intent)
-            finish() // Selesai dengan menutup StageActivity
-
-        } else if (selectedId == R.id.rb_ortu) {
-            // Navigasi ke Activity Biodata anak
-            val intent = Intent(this, BioActivity::class.java)
-            startActivity(intent)
-            finish() // Selesai dengan menutup StageActivity
-        }
-    }
-
-    private fun getSelectedRadioButtonId(stage: String): Int? {
-        return when (stage) {
-            "Calon Orang Tua" -> R.id.rb_calon
-            "Orang Tua" -> R.id.rb_ortu
-            else -> null
+            } else if (selectedId == R.id.rb_ortu) {
+                // Navigasi ke Activity Biodata anak
+                val intent = Intent(this, BioActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
-
